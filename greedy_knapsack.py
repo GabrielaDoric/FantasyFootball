adf5334360750faf99807eba88a54f6c61b2e98e
Gabriela <gabriela.doric@fer.hr>
2020-11-08 12:32:29 +0100

    Finally implemented

--- greedy_knapsack.py
+++ greedy_knapsack.py
@@ -5,7 +5,6 @@ from operator import itemgetter
 PATH_TO_CSV = './instances/instance1.csv'
 
 
-
 def pick_worst_4(df):
     worst_players = []
 
@@ -25,66 +24,154 @@ def pick_worst_4(df):
 def pick_n_best_players(df):
     best_players = []
 
-    best_gk = df.loc[(df['Position'] == 'GK')].nlargest(20, ['Price']).values.tolist()
+    best_gk = df.loc[(df['Position'] == 'GK')].nlargest(5, ['Price']).values.tolist()
     best_players.extend(best_gk)
 
-    best_def = df.loc[(df['Position'] == 'DEF')].nlargest(20, ['Price']).values.tolist()
+    best_def = df.loc[(df['Position'] == 'DEF')].nlargest(10, ['Price']).values.tolist()
     best_players.extend(best_def)
 
-    best_fw = df.loc[(df['Position'] == 'FW')].nlargest(20, ['Price']).values.tolist()
+    best_fw = df.loc[(df['Position'] == 'FW')].nlargest(10, ['Price']).values.tolist()
     best_players.extend(best_fw)
 
-    best_mid = df.loc[(df['Position'] == 'MID')].nlargest(15, ['Price']).values.tolist()
+    best_mid = df.loc[(df['Position'] == 'MID')].nlargest(20, ['Price']).values.tolist()
     best_players.extend(best_mid)
 
     # print (np.array(worst_players))
-    return best_players
+
+    return best_gk, best_def, best_fw, best_mid, best_players
 
 
-def greedy_knapsack(best_players,prices, points, W, n):
-    lineup=[]
-    lineup_price=0
-    lineup_points=0
+def greedy_knapsack(best_players, prices, points, W, n):
+    lineup = []
+    lineup_price = 0
+    lineup_points = 0
+    counter = 0
+    taken_positions = []
+    clubs_from = []
 
-    counter=0
-    taken_positions=[]
-    clubs_from=[]
+    # best_players = points_per_million(best_players)
+    # sorted_players = sorted(best_players, key=lambda x: x[6])
 
+    # best_players = points_per_million(best_players)
     sorted_players = sorted(best_players, key=lambda x: x[4])
-    #print (sorted_players)
-    #sorted_players= (sorted_players)
-    #print (type(sorted_players))
 
-    while len(sorted_players)>0 and counter<11:
+    #print(np.array(sorted_players))
+
+    while len(sorted_players) > 0 and counter <= 11:
         player = sorted_players.pop()
-        id,position,name,club,points, price=player
+        # print (len(sorted_players))
+        id, position, name, club, points, price = player
 
-
-        if (position == 'GK' and taken_positions.count('GK') == 1) or (position == 'DEF' and taken_positions.count('DEF') == 3) \
-                    or (position == 'MID' and taken_positions.count('MID') == 5) or (position == 'FW' and taken_positions.count('FW') == 2)\
-                    or (clubs_from.count(club) == 3):
+        if (position == 'GK' and taken_positions.count('GK') == 1) or (
+                position == 'DEF' and taken_positions.count('DEF') == 3) \
+                or (position == 'MID' and taken_positions.count('MID') == 5) or (
+                position == 'FW' and taken_positions.count('FW') == 2) \
+                or (clubs_from.count(club) == 3):
             continue
 
+        #print (84.1 - (price + lineup_price))
+        #print ((len(taken_positions)+1)* 4.5)
+        if (84.1 - (price + lineup_price)) < (11-(len(taken_positions)+1)) * 4.5:
+            continue
 
-        if price + lineup_price <= W:
-            counter+=1
+        if price + lineup_price <= 84.1:
+
+            counter += 1
             lineup.append(player)
-            lineup_price+=price
-            lineup_points+=points
+            lineup_price += price
+            lineup_points += points
             taken_positions.append(position)
             clubs_from.append(club)
+
         else:
             break
 
-    #print (taken_positions)
-    #print (clubs_from)
+    # print (taken_positions)
+    # print (clubs_from)
+
+    print ('lineup_price',lineup_price)
+
+    # best_players_sorted=sorted(best_players,key=)
+
+    #for player in lineup:
+    #    player.pop()
+
+    print(len(lineup))
+
+    return lineup, lineup_points
 
 
+def pokusaj(best_gk, best_def, best_fw, best_mid, best_players, prices, points, W, n):
+    CONSTRAINT_GK = 1
+    CONSTRAINT_MID = 5
+    CONSTRAINT_FW = 2
+    CONTRAINT_DEF = 3
+
+    sorted_gk = sorted(best_gk, key=lambda x: x[4])
+    sorted_def = sorted(best_def, key=lambda x: x[4], reverse=True)
+    sorted_fw = sorted(best_fw, key=lambda x: x[4], reverse=True)
+    sorted_mid = sorted(best_mid, key=lambda x: x[4])
+
+    lineup = []
+    lineup_price = 0
+    lineup_points = 0
+
+    counter = 0
+    taken_positions = []
+    clubs_from = []
+
+    # izaberi gk
+    lineup.append(sorted_gk.pop())
+
+    while counter < CONSTRAINT_MID:
+        counter += 1
+        lineup.append(sorted_mid.pop())
+
+    counter = 0
+    while counter < CONTRAINT_DEF:
+        counter += 1
+        lineup.append(sorted_def.pop())
+
+    counter = 0
+    while counter < CONSTRAINT_FW:
+        counter += 1
+        lineup.append(sorted_fw.pop())
+
+    trosak = 0
+    for player in lineup:
+        trosak += player[5]
+
+    print(trosak)
+
+    print(np.array(lineup))
 
 
-    #best_players_sorted=sorted(best_players,key=)
+def points_per_million(players):
+    for player in players:
+        ppm = player[4] / player[5]
+        player.append(ppm)
 
-    return lineup,lineup_points
+    return players
+
+
+def pokusaj_1(best_gk, best_def, best_fw, best_mid, best, prices, points, W, n):
+    # print (type(best_gk))
+    # print (np.array(best_gk))
+
+    best = points_per_million(best)
+    # print(np.array(best))
+
+    sorted_players = sorted(best, key=lambda x: x[6])
+    print(np.array(sorted_players))
+
+
+def df_to_list(df, lista_najgorih):
+    nova_lista = df.values.tolist()
+
+    res = list(filter(lambda i: i not in lista_najgorih, nova_lista))
+
+    return res
+
 
 if __name__ == '__main__':
     lineup = []
@@ -104,11 +191,12 @@ if __name__ == '__main__':
 
     lineup.extend(worst_4)
 
-    best = pick_n_best_players(df)
+    best_gk, best_def, best_fw, best_mid, best = pick_n_best_players(df)
     # print((np.array(best)))
 
-    he = np.array(best)
-    # print (he[0][1])
+    #print('sto je worst_4', type(worst_4))
+    lista_svih = df_to_list(df, worst_4)
+    # print (lista_svih)
 
     points = []
     prices = []
@@ -119,23 +207,11 @@ if __name__ == '__main__':
     val = points
     wt = prices
     W = 1000 - spent_money * 10
-
     n = len(val)
 
-    # print (n)
-    # print (W)
-
-
-    #player_indices, rez = knapsack1(W, wt, val, n, np.array(best))
-    #print (rez)
-    #for j in player_indices:
-    #    print (best[j-1])
-
-    # print (np.array(best))
-
-    igraci, dobit = greedy_knapsack(best,prices, points, W, n)
-    print (dobit)
-    print (np.array(igraci))
+    igraci, dobit = greedy_knapsack(lista_svih, prices, points, W, n)
+    print(dobit)
+    print(np.array(igraci))
 
     '''
     lineup=np.array(lineup)
@@ -147,5 +223,5 @@ if __name__ == '__main__':
     print (sorted)
     '''
 
-
-
+    # pokusaj = pokusaj_1(best_gk, best_def, best_fw, best_mid, best, prices, points, W, n)
+    # print (pokusaj)
